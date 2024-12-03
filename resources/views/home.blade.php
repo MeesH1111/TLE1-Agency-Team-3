4f932b17a901faa42df85903740668f4d340a1a7
nbjjans-pixel <1056796@hr.nl>
2024-12-03 11:33:26 +0100

    test commit voor error

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Open Hiring</title>
+    @vite('resources/css/homepagestyle.css')
+</head>
+<body>
+
+<div class="container">
+    <section class="introductie">
+        <h1>Een baan zonder sollicitatiegesprek</h1>
+        <div class="stappen">
+            <div>Direct reageren, zonder sollicitatiegesprek</div>
+            <div>Jij bepaalt of je het kunt</div>
+            <div>Snel aan de slag, met normaal contract</div>
+        </div>
+        <a href="#" class="btn">Meer weten?</a>
+    </section>
+
+    <section class="partners">
+        <h2>Onze partners</h2>      <!-- naam misschien aanpassen partners is moeilijk te begrijpen? -->
+        <div class="partner-logos">
+            <p>Hier komen wat lokale rotterdamse kleine bedrijven</p>
+        </div>
+    </section>
+
+    <!-- succesverhaal -->
+    <section class="succesverhaal">
+        <h2>Van doorzetter naar inspiratie</h2>
+        <p>Na een lange zoektocht naar werk vond Nico, door Open Hiring, een kans bij WERKPLEK NOG ONBEKEND.</p>
+        <a href="#" class="btn">Bekijk het verhaal</a>
+    </section>
+</div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/test', function () {
-    return view('test');
-});
+Route::get('/home', function () { return view('home');})->name('home');
 
 require __DIR__.'/auth.php';
