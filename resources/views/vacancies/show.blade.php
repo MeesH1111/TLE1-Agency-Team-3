07777c0d30feef1e555664c4278b6da4532263a7
SemvanOsch <145602882+SemvanOsch@users.noreply.github.com>
2024-12-04 22:50:49 +0100

    Added css

--- resources/views/vacancies/show.blade.php
+++ resources/views/vacancies/show.blade.php
@@ -5,22 +5,36 @@
     </x-slot:title>
     {{--    Link hier naar de css pagina die je wilt gebruiken--}}
     <x-slot name="css">
-        @vite('resources/css/app.css')
-        @vite('resources/css/vacancy.css')
+        @vite('resources/css/vacancyDetails.css')
     </x-slot>
 
-    <h1 class="text-2xl font-bold">{{ $vacancy->role }}</h1>
-
-    <p>Bedrijf: {{$vacancy->company->name}}</p>
-    <p>Salaris: {{ $vacancy->salary }}</p>
-    <p>Uren: {{ $vacancy->hours }}</p>
-    <p>Locatie: {{ $vacancy->location }}</p>
-    <p>Type: {{ $vacancy->type }}</p>
-    <p>Benodigdheden: {{ $vacancy->requirements }}</p>
-    <p>Beschrijving: {{ $vacancy->description }}</p>
-    <a href="{{ route('vacancies.index', $vacancy->category_id)}}"
-       class="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600">
-        Terug naar vacatures
-    </a>
+    <div class="vacancy-header">
+        <img src="" alt="{{ $vacancy->company->name }} Logo">
+        <div class="vacancy-divider-head">
+            <h1>{{ $vacancy->company->name }}</h1>
+            <h2>{{ $vacancy->role }}</h2>
+        </div>
+    </div>
+    <div class="vacancy-divider-main">
+        <div class="vacancy-info">
+            <div><p><span>Salaris:</span></p>
+                <p>{{ $vacancy->salary }} per maand</p></div>
+            <div><p><span>Uren:</span></p>
+                <p>{{ $vacancy->hours }} per week</p></div>
+            <div><p><span>Locatie:</span></p>
+                <p>{{ $vacancy->location }}</p></div>
+            <div><p><span>Type:</span></p>
+                <p>{{ $vacancy->type }}</p></div>
+            <div><p><span>Benodigdheden:</span></p>
+                <p>{{ $vacancy->requirements }}</p></div>
+        </div>
+        <div class="vacancy-description">
+            {{ $vacancy->description }}
+        </div>
+    </div>
+    <div class="vacancy-buttons">
+        <a href="{{ route('vacancies.index', $vacancy->category_id)}}" class="back" aria-label="Terug naar Vacatures">Terug</a>
+        <a href="#" class="apply" aria-label="Reageer op deze vacature">Reageer</a>
+    </div>
 
 </x-base-layout>
