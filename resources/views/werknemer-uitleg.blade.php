1df409db172167c0b2f0bb05bfddc8b95d038f0e
meesh <meeshoff2006@gmail.com>
2024-12-04 10:19:27 +0100

    Updated werknemer uitlegpagina

--- resources/views/werknemer-uitleg.blade.php
+++ resources/views/werknemer-uitleg.blade.php
@@ -1,56 +1,67 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/werknemeruitleg.css')
-    <title>Werknemer Uitlegpagina</title>
-</head>
-<body>
-<main>
+<x-base-layout>
+    {{--    Zet hier de title van je pagina neer--}}
+    <x-slot:title>
+        Werknemer uitlegpagina
+    </x-slot:title>
+    {{--    Link hier naar de css pagina die je wilt gebruiken--}}
+    <x-slot name="css">
+
+    </x-slot>
     <div class="container">
-        <a href="" class="backarrow">Back Arrow</a>
-        <h2>Hoe het werkt als werkzoekende</h2>
-        <img src="/images/werknemeruitlegimage1.png" alt="twee werknemers in de bouw">
+        <a href="{{ route('home') }}" class="backarrow"></a>
+        <div class="containerinfo">
+            <h2 class="title">Hoe het werkt als werkzoekende</h2>
+            <img class="uitlegimg" src="/images/werknemeruitlegimage1.png" alt="twee werknemers in de bouw">
 
-        <p>Lorem ipsum odor amet, consectetuer adipiscing elit.
-            Cursus risus habitasse est imperdiet ante ultrices pharetra tortor.
-            Condimentum sagittis aliquet penatibus eros montes.
-            Urna montes eleifend libero quam ligula consectetur tortor hac.
-        </p>
+            <p class="werknemertekst">Lorem ipsum odor amet, consectetuer adipiscing elit.
+                Cursus risus habitasse est imperdiet ante ultrices pharetra tortor.
+                Condimentum sagittis aliquet penatibus eros montes.
+                Urna montes eleifend libero quam ligula consectetur tortor hac.
+            </p>
+        </div>
+
+        <div class="containerinfo">
+            <h2 class="title">Maak een account</h2>
+            <img class="uitlegimg" src="/images/werknemeruitlegimage2.png" alt="">
+            <p class="werknemertekst">
+                Lorem ipsum odor amet, consectetuer adipiscing elit.
+                Cursus risus habitasse est imperdiet ante ultrices pharetra tortor.
+                Condimentum sagittis aliquet penatibus eros montes.
+                Urna montes eleifend libero quam ligula consectetur tortor hac.
+            </p>
+            <a class="btn" href="">Account aanmaken</a>
+        </div>
+
+        <div class="containerinfo">
+            <h2 class="title">Zoek een vacature</h2>
+            <p class="werknemertekst">
+                Lorem ipsum odor amet, consectetuer adipiscing elit.
+                Cursus risus habitasse est imperdiet ante ultrices pharetra tortor.
+                Condimentum sagittis aliquet penatibus eros montes.
+                Urna montes eleifend libero quam ligula consectetur tortor hac.
+            </p>
+            <a class="btn" href="">Zoek vacatures</a>
+        </div>
+
+        <div class="storycointaner">
+            <p class="storytitle">Van doorzetter naar inspiratie</p>
+            <div class="storyinfo">
+                <p class="storyp">
+                    Na een lange zoektocht naar werk vond Nico, door Open Hiring en ondanks de uitdagingen van zijn
+                    rolstoel,
+                    een
+                    kans bij KFC.
+                </p>
+                <div class="storyimgcontainer">
+                    <img class="storyimg" src="images/werknemeruitlegimage3.png"
+                         alt="Nico die buiten in een rolstoel zit">
+                </div>
+            </div>
+            <a class="btn" href="">Bekijk verhaal</a>
+
+
+        </div>
     </div>
+</x-base-layout>
 
-    <div class="container">
-        <h2>Maak een account</h2>
-        <img src="/images/werknemeruitlegimage2.png" alt="">
-        <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit.
-            Cursus risus habitasse est imperdiet ante ultrices pharetra tortor.
-            Condimentum sagittis aliquet penatibus eros montes.
-            Urna montes eleifend libero quam ligula consectetur tortor hac.
-        </p>
-        <a class="button" href="">Account aanmaken</a>
-    </div>
-
-    <h2>Zoek een vacature</h2>
-    <p>
-        Lorem ipsum odor amet, consectetuer adipiscing elit.
-        Cursus risus habitasse est imperdiet ante ultrices pharetra tortor.
-        Condimentum sagittis aliquet penatibus eros montes.
-        Urna montes eleifend libero quam ligula consectetur tortor hac.
-    </p>
-    <a class="button" href="">Zoek vacatures</a>
-
-    <p>Van doorzetter naar inspiratie</p>
-    <p>
-        Na een lange zoektocht naar werk vond Nico, door Open Hiring en ondanks de uitdagingen van zijn rolstoel, een
-        kans bij KFC.
-    </p>
-    <img src="images/werknemeruitlegimage3.png" alt="Nico die buiten in een rolstoel zit">
-
-</main>
-</body>
-</html>
 
