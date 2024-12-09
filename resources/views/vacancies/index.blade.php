7d91c8f98337a44cf8002d75cb99324632fe4bf9
J-Hoogvliet <justinhoogvliet@outlook.com>
2024-12-09 15:05:51 +0100

    Cards tonen met uitspringende onderdelen

--- resources/views/vacancies/index.blade.php
+++ resources/views/vacancies/index.blade.php
@@ -13,28 +13,30 @@
 
     <h1 id="vacancyTitle">Vacatures</h1>
     <h2 id="vacancyUnderTitle" class="{{$categoryModel->color}}">{{ $categoryModel->name }}</h2>
-    <a href="{{ route('categories.index') }}" class="backarrow"></a>
-
-
+    <div id="grieks">
+        <a href="{{ route('categories.index') }}" class="backarrow"></a>
+        <p>Terug naar categorieën</p>
+    </div>
     <form method="get" action="{{route('vacancies.search')}}">
         @csrf
         <label for="category"></label>
         <input type="hidden" name="category" id="category" value="{{$category}}">
 
         <label for="search"></label>
-        <input type="text" name="search" id="search" placeholder="Zoek specifiek"
+        <input type="text" name="search" id="search" class="groen" placeholder="Zoek specifiek"
                value="{{ request()->input('searchToken') ?  request()->input('searchToken') : ''  }}">
     </form>
-    <div id="GriekseY" class="{{$categoryModel->color}}">
+    <div id="cardsContainer" class="{{$categoryModel->color}}">
         @if($vacancies->isNotEmpty())
             <div id="vacancy-list">
                 @foreach ($vacancies as $vacancy)
                     <ul>
-                        <li>Rol: {{ $vacancy->role}} </li>
-                        <li>Type: {{ $vacancy->type}} </li>
-                        <li>Salaris: €{{ $vacancy->salary}} per maand</li>
-                        <li>Uren: {{ $vacancy->hours}} per week</li>
-                        <a href="{{route('vacancies.show', ['id' => $vacancy->id])}}">Zie details</a>
+                        <li id="function">Functie: <span class="functionSpan">{{ $vacancy->role}}</span></li>
+                        <li>Locatie: {{ $vacancy->company->name}}</li>
+                        <li id="wage">Salaris: €{{ $vacancy->salary}} per maand</li>
+                        <li>Aantal uren per week: {{ $vacancy->hours}}</li>
+                        <a class="ulA{{$categoryModel->color}}"
+                           href="{{route('vacancies.show', ['id' => $vacancy->id])}}">Zie Meer !</a>
                     </ul>
                 @endforeach
             </div>
