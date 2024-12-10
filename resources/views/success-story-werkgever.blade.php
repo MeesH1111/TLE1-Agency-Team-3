c901fc1c0fa8b042748207a3662cba20a8bfcc45
meesh <meeshoff2006@gmail.com>
2024-12-10 12:13:26 +0100

    Added werknemer success story and buttons

--- resources/views/success-story-werkgever.blade.php
+++ resources/views/success-story-werkgever.blade.php
@@ -19,8 +19,8 @@
                 Open Hiring bleek meer dan een tijdsbesparing. Tanja’s bedrijfscultuur verbeterde door de diversiteit en motivatie van medewerkers die hun kans grepen, ongeacht hun achtergrond. Dankzij de vrijgekomen tijd richtte Tanja zich op groei. Ze lanceerde een glutenvrije productlijn, sloot een deal met een supermarktketen en gaf workshops over duurzaam bakken.            </p>
             <p>
                 In twee jaar tijd verdubbelde haar omzet en het aantal werknemers. Tanja’s aanpak inspireerde andere bedrijven in de regio. "Ik dacht dat mijn groei zou stagneren door het personeelstekort, maar Open Hiring gaf me juist een duw vooruit. Nu draag ik niet alleen bij aan een succesvolle bakkerij, maar ook aan een inclusieve samenleving."            </p>
-            <img src="/images/succestanja.png" alt="Blije man in een rolstoel.">
-            <a href="{{ route('register') }}" class="btn" id="story-button">Account maken</a>
+            <img src="/images/succestanja.png" alt="Blije bedrijfseigenaar">
+            <a href="{{ route('register') }}" class="btn" id="story-button" aria-label="Knop om een account aan te maken">Account maken</a>
         </section>
     </div>
 
