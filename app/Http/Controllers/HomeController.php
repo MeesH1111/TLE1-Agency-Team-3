2c6c4a372a25314b0b04a9d98a4c0f50354aa6d7
meesh <meeshoff2006@gmail.com>
2024-12-12 11:51:34 +0100

    Fixed home page companies

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
 
         // Hier kun je handmatig een IP-adres instellen voor de demo
         $manualIp = '185.156.172.142'; // Vervang dit door je gewenste IP
-        $useManualIp =false; // Zet op false om de normale methode te gebruiken
+        $useManualIp =true; // Zet op false om de normale methode te gebruiken
 
         if ($useManualIp) {
             $ip = $manualIp;
@@ -32,20 +32,20 @@ class HomeController extends Controller
         }
 
         $partnerImages = [
-            ['path' => 'images/fitshelogo.png', 'alt' => 'Logo van Fitshe', 'id' => 1],  //id nog aanpassen dit is een tijdelijke oplossing
-            ['path' => 'images/krekerij.jpg', 'alt' => 'Logo van de Krekerij', 'id' => 2],
-            ['path' => 'images/svh.png', 'alt' => 'Logo van SVH', 'id' => 3],
-            ['path' => 'images/toettoetfood.png', 'alt' => 'Logo van ToetToetFood', 'id' => 4],
+            ['path' => 'images/fitshelogo.png', 'alt' => 'Logo van Fitshe', 'id' => 11],  //id nog aanpassen dit is een tijdelijke oplossing
+            ['path' => 'images/krekerij.jpg', 'alt' => 'Logo van de Krekerij', 'id' => 12],
+            ['path' => 'images/svh.png', 'alt' => 'Logo van SVH', 'id' => 13],
+            ['path' => 'images/toettoetfood.png', 'alt' => 'Logo van ToetToetFood', 'id' => 14],
         ];
 
 
         if ($ip === '185.156.172.142') {
             $locatie = "Amsterdam";
             $partnerImages = [
-                ['path' => 'images/bbrood.png', 'alt' => 'Logo van bbrood', 'id' => 5],  //id nog aanpassen dit is een tijdelijke oplossing
-                ['path' => 'images/duurzamemode.png', 'alt' => 'Logo van de duurzamemode', 'id' => 6],
-                ['path' => 'images/SPREZZAT.png', 'alt' => 'Logo van SPREZZAT', 'id' => 7],
-                ['path' => 'images/vandekoe.png', 'alt' => 'Logo van vandekoe', 'id' => 8],
+                ['path' => 'images/bbrood.png', 'alt' => 'Logo van bbrood', 'id' => 15],  //id nog aanpassen dit is een tijdelijke oplossing
+                ['path' => 'images/duurzamemode.png', 'alt' => 'Logo van de duurzamemode', 'id' => 16],
+                ['path' => 'images/SPREZZAT.png', 'alt' => 'Logo van SPREZZAT', 'id' => 17],
+                ['path' => 'images/vandekoe.png', 'alt' => 'Logo van vandekoe', 'id' => 18],
             ];
         }
 
