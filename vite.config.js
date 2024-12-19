d0c73089a900acfbc6cd8957df3f431e50ea3527
meesh <meeshoff2006@gmail.com>
2024-12-19 12:05:13 +0100

    Vite change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/main.js', 'resources/css/aboutUs.css', 'resources/css/categories.css', 'resources/css/login.css', 'resources/css/succes.css', 'resources/css/vacancy.css', 'resources/css/vacancyDetails.css', 'resources/css/werknemeruitleg.css', "resources/css/companies.css", "resources/css/vacanciesCreate.css"],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/main.js', 'resources/css/aboutUs.css', 'resources/css/categories.css', 'resources/css/login.css', 'resources/css/succes.css', 'resources/css/vacancy.css', 'resources/css/vacancyDetails.css', 'resources/css/werknemeruitleg.css', "resources/css/companies.css", "resources/css/vacanciesCreate.css", "resources/js/companies.js"],
             refresh: true,
         }),
     ],
